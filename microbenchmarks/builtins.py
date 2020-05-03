57a488de1a8a8062e71af08d7194e3a2ff936743
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-05-03 15:09:09 +0100

    Add builtins microbenchmarks

---
+++ microbenchmarks/builtins.py
@@ -0,0 +1,42 @@
+"""Benchmarks for built-in functions (that don't fit elsewhere)."""
+
+from benchmarking import benchmark
+
+
+@benchmark
+def min_max_pair() -> None:
+    a = []
+    for i in range(20):
+        a.append(i * 12753 % (2**15 - 1))
+
+    expected_min = min(a)
+    expected_max = max(a)
+
+    n = 0
+    for i in range(100 * 1000):
+        n = 1000000000
+        m = 0
+
+        for j in a:
+            n = min(n, j)
+            m = max(m, j)
+        assert n == expected_min
+        assert m == expected_max
+
+
+@benchmark
+def min_max_sequence() -> None:
+    a = []
+    for i in range(1000):
+        a.append([i * 2])
+        a.append([i, i + 2])
+        a.append([i] * 15)
+
+    n = 0
+    for i in range(100):
+        for s in a:
+            x = min(s)
+            n += x
+            x = max(s)
+            n += x
+    assert n == 399800000, n
