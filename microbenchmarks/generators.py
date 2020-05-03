d5a21d0f067f8cd79493cdb55a5cc98956caa036
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-05-03 15:26:17 +0100

    Add generators microbenchmark

---
+++ microbenchmarks/generators.py
@@ -0,0 +1,21 @@
+from typing import Iterator
+
+from benchmarking import benchmark
+
+
+@benchmark
+def generators() -> None:
+    n = 0
+    k = 0
+    for i in range(100 * 1000):
+        for j in gen(k):
+            n += j
+        k += 1
+        if k == 10:
+            k = 0
+    assert n == 1200000, n
+
+
+def gen(n: int) -> Iterator[int]:
+    for i in range(n):
+        yield i
