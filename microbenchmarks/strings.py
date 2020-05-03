2e7c95a29c41bc8c5fdb920c70b65aa2f1bff5e2
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-05-03 15:37:49 +0100

    Add strings microbenchmarks

---
+++ microbenchmarks/strings.py
@@ -0,0 +1,163 @@
+from benchmarking import benchmark
+
+
+@benchmark
+def str_methods() -> None:
+    """Use a mix of str methods (but not split/join)."""
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('  %d str' % i)
+
+    n = 0
+    for i in range(100):
+        for s in a:
+            if s.startswith('foo'):
+                n += 1
+            if s.endswith('r'):
+                n += 1
+            if s.replace('-', '/') != s:
+                n += 1
+            if s.strip() != s:
+                n += 1
+            if s.rstrip() != s:
+                n += 1
+            if s.lower() == s:
+                n += 1
+    assert n == 400000, n
+
+
+@benchmark
+def str_format() -> None:
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('%d str' % i)
+
+    n = 0
+    for i in range(100):
+        for s in a:
+            n += len("foobar {} stuff".format(s))
+            ss = "foobar %s stuff" % s
+            n += len("%s-%s" % (s, ss))
+    assert n == 10434000, n
+
+
+@benchmark
+def str_slicing() -> None:
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('%d str' % i)
+
+    n = 0
+    for i in range(1000):
+        for s in a:
+            n += len(s[2:-2])
+            if s[:3] == 'Foo':
+                n += 1
+            if s[-2:] == '00':
+                n += 1
+    assert n == 9789000, n
+
+
+@benchmark
+def split_and_join() -> None:
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('%d-ab-asdfsdf-asdf' % i)
+        a.append('yeah')
+    n = 0
+    for i in range(100):
+        for s in a:
+            items = s.split('-')
+            if '-'.join(items) == s:
+                n += 1
+    assert n == 300000, n
+
+
+@benchmark
+def encode_decode() -> None:
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('%d-ab-asdfsdf-asdf' % i)
+        a.append('yeah')
+    n = 0
+    for i in range(100):
+        for s in a:
+            b = s.encode("ascii")
+            if b.decode("ascii") != s:
+                n += 1
+
+            b = s.encode("utf8")
+            if b.decode("utf8") == s:
+                n += 1
+    assert n == 300000, n
+
+
+@benchmark
+def str_searching() -> None:
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('%d-ab-asdfsdf-asdf' % i)
+        a.append('yeah')
+    n = 0
+    for i in range(100):
+        for s in a:
+            if 'i' in s:
+                n += 1
+            if s.find('asd') >= 0:
+                n += 1
+            n += s.index('a')
+    assert n == 1089000, n
+
+
+@benchmark
+def str_call() -> None:
+    a = []
+    for i in range(100):
+        a.append(Cls(i))
+
+    n = 0
+    for i in range(10 * 1000):
+        for obj in a:
+            s1 = str(obj)
+            s2 = str(s1)  # No-op
+            n += len(s2)
+
+    assert n == 1900000, n
+
+
+class Cls:
+    def __init__(self, x: int) -> None:
+        self.x = x
+
+    def __str__(self) -> str:
+        return str(self.x)
+
+
+@benchmark
+def ord_builtin() -> None:
+    a = []
+    for i in range(1000):
+        a.append('Foobar-%d' % i)
+        a.append('%d-ab-asdfsdf-asdf' % i)
+        a.append('yeah')
+    n = 0
+    for i in range(50):
+        for s in a:
+            for j in range(len(s)):
+                if 97 <= ord(s[j]) <= 122:
+                    n += 1
+                if is_upper_case_letter(s[j]):
+                    n += 2
+                if s[j] == ord('a'):
+                    n += 3
+    assert n == 1200000, n
+
+
+def is_upper_case_letter(ch: str) -> bool:
+    return 65 <= ord(ch) <= 90
