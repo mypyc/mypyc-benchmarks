60d1a6d47b5da5411792533cfa7922cd3472fc23
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-05-09 12:37:22 +0100

    Add tuning script for Linux

---
+++ scripts/tune.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Tune Linux for more repeatable measurements.
+# This assumes a 4-core/8-thread CPU.
+#
+# Adapted from https://easyperf.net/blog/2019/08/02/Perf-measurement-environment-on-Linux
+
+# Disable address space randomization
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+
+# Use performance scaling governor
+for f in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+do
+    echo performance | sudo tee $f
+done
+
+# Disable hyperthreading
+for f in /sys/devices/system/cpu/cpu[4567]/online
+do
+    echo 0 | sudo tee $f
+done
