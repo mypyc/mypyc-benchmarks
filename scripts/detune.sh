9b819d579fde4644b9a6112d244bb645db082d84
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2020-05-09 13:16:41 +0100

    Add detune script to disable tunings

---
+++ scripts/detune.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Disable Linux tunings for more repeatable measurements.
+# This assumes a 4-core/8-thread CPU.
+
+# Enable hyperthreading
+for f in /sys/devices/system/cpu/cpu[4567]/online
+do
+    echo 1 | sudo tee $f
+done
+
+# Enable address space randomization
+echo 2 | sudo tee /proc/sys/kernel/randomize_va_space
+
+# Use performance scaling governor
+for f in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+do
+    echo powersave | sudo tee $f
+done
