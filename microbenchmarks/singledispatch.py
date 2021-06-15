5457dc52e8b0fb11173c67e4f059ea12a4b046e2
Pranav Rajpal <78008260+pranavrajpal@users.noreply.github.com>
2021-06-15 12:45:03 -0700

    Rename benchmark to sum_tree_singledispatch
    Rename the benchmark to emphasize that it's profiling the performance of
    singledispatch.

--- microbenchmarks/singledispatch.py
+++ microbenchmarks/singledispatch.py
@@ -42,7 +42,7 @@ def build(n: int) -> Tree:
 
 
 @benchmark
-def sum_tree():
+def sum_tree_singledispatch():
     # making the tree too big causes building the tree to take too long or just crash python entirely
     tree = build(10)
     n: int = 0
